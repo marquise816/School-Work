383cf5e4416b25d74b98d30f3ebde70602832904
Marquise Trawick <marquisetrawick@gmail.com>
2023-08-23 16:41:44 -0400

    Computer Graphics

---
+++ 3D Image from Dat Files/Point3D.java
@@ -0,0 +1,7 @@
+// Point3D.java: Representation of a point in 3D space.
+class Point3D {
+   float x, y, z;
+   Point3D(double x, double y, double z) {
+      this.x = (float) x; this.y = (float) y; this.z = (float) z;
+   }
+}
